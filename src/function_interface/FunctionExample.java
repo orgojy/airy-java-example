71d22fba74b2ef8ca37a2573d7cc171b318054cb
Ryan-An <giveawesome@gmail.com>
2020-05-05 15:10:36 +0900

    Add example for Functional Interface "Function"

---
+++ src/function_interface/FunctionExample.java
@@ -0,0 +1,42 @@
+package function_interface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <p> Functional Interface "Function"
+ * <p> : To use "Behavior parameters", Use "Functional Interface"
+ * <p> - You can implement dynamic parameters for behavior by using "Functional Interface"
+ * <p> - You can check validation by declaring "@FunctionalInterface"
+ * <p> - To be used for returning values by lambda expression, Use "Function"
+ * <p> - Implemented interface {@link java.util.function.Function}
+ *
+ * @author Yimin An
+ */
+public class FunctionExample {
+
+  @FunctionalInterface
+  public interface Function<T, R> {
+
+    R apply(T t);
+  }
+
+  public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
+    List<R> result = new ArrayList<>();
+    for (T s : list) {
+      result.add(f.apply(s));
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Function<String, Integer> lengthFunction = (String s) -> s.length();
+
+    List<Integer> integerList = FunctionExample.map(
+        Arrays.asList("a", "bb", "ccc", "dddd"),
+        lengthFunction
+    );
+    integerList.forEach(System.out::println);
+  }
+}
