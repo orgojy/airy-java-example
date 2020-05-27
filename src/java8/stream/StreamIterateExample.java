ac1a1fc353302aa593c0cff44d37c0d828218c9f
Ryan An <giveawesome@gmail.com>
2020-05-27 20:02:33 +0900

    Add example for iterate of stream

---
+++ src/java8/stream/StreamIterateExample.java
@@ -0,0 +1,28 @@
+package java8.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * <p> Utility method "iterate" of Stream
+ * : Use to get stream from iterative logic
+ *
+ * @author Yimin An
+ */
+public class StreamIterateExample {
+
+  public static void main(String[] args) {
+
+    // Example : Arithmetic sequence
+    Stream.iterate(0, n -> n + 2)
+        .limit(10)
+        .forEach(n -> System.out.print(n + " "));
+    System.out.println();
+
+    // Example : Fibonacci numbers
+    Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+        .limit(10)
+        .map(t -> t[0])
+        .forEach(t -> System.out.print(t + " "));
+    System.out.println();
+  }
+}
